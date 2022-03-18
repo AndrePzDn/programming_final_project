2e868d90890c9c24d594c4890aa97c1558254748
PradoJosue <prado69508698@gmail.com>
2022-03-18 12:47:42 -0400

    Add Windows

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import users.User;
 import users.UserAdmin;
 import users.UserClient;
 import users.BasicFunctionOfUsers;
-
+import Windows.Window1;
 public class App {
     public static void main(String[] args){
         BasicFunctionOfUsers ls = new BasicFunctionOfUsers();
@@ -16,5 +16,8 @@ public class App {
             UserAdmin userAdmin = (UserAdmin) us;
             System.out.println(userAdmin.getName());
         }
+	Window1 Interface1 = new Window1();
+        
+        Interface1.setVisible(true);
     }
 }
