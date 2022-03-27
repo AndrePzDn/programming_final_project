a0eef5280f97c231987b2a31ea3ea9069dbedc19
AndrePzDn <andreperezduran@gmail.com>
2022-03-27 14:30:48 -0400

    Add logIn with database sqlite

--- src/InterfaceVr2/App.java
+++ src/InterfaceVr2/App.java
@@ -9,7 +9,6 @@ public class App {
 
     public static void main(String[] args) {
         Login Interface1 = new Login();
-        
         Interface1.setVisible(true);
     }
 }
--- src/users/BasicFunctionOfUsers.java
+++ src/users/BasicFunctionOfUsers.java
@@ -1,5 +1,10 @@
 package users;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BasicFunctionOfUsers {
@@ -10,54 +15,51 @@ public class BasicFunctionOfUsers {
         ArrayList<User> list = new ArrayList<User>();
         this.userList = list;
     }
-    
-    public ArrayList<User> instantiateUsers(){
-        User user0 = new UserAdmin(0, "vebek33545@tonaeto.com", "Thiago Galan", "sinDavoL", "WrathCharge", "Male");
-        User user1 = new UserAdmin(1, "venojeihuta-5144@yopmail.com", "Valentin Morera", "OmBeiTyc", "NarrowVictory", "Male");
-        User user2 = new UserAdmin(2, "biprullamiffo-8039@yopmail.com", "Vicente Alvarado", "ETANTHEm", "ViperStrike", "Male");
-        User user3 = new UserClient(3, 0, 0, "braciwitefa-6891@yopmail.com", "Sheila Planas", "PeaseYwe", "HereSheIs", "Female");
-        User user4 = new UserClient(4, 0, 0, "vazauppusore-4510@yopmail.com", "Nuria Alamo", "ICHEBArD", "BellBoy", "Female");
-        User user5 = new UserClient(5, 0, 0, "xauroxaroiffa-6527@yopmail.com", "Emílio Sánchez", "alISIANd", "FlowerPower", "Male");
-        User user6 = new UserClient(6, 0, 0, "zeuddijugregre-5207@yopmail.com", "Amanda Sainz", "bRACtiOu", "Kittywake", "Female");
-        User user7 = new UserClient(7, 0, 0, "mipauketreufro-4850@yopmail.com", "Julieta Andrade", "FICHembn", "ParasiteTown", "Female");
-        User user8 = new UserClient(8, 0, 0, "woussotteubregru-6255@yopmail.com", "Eduard Yague", "AVerTUpa", "Blacklight", "Male");
-        User user9 = new UserClient(9, 0, 0, "praffaniceddau-2776@yopmail.com", "Erik Gamero", "TabySELi", "Safflower", "Male");
-
-        this.userList.add(user0);
-        this.userList.add(user1);
-        this.userList.add(user2);
-        this.userList.add(user3);
-        this.userList.add(user4);
-        this.userList.add(user5);
-        this.userList.add(user6);
-        this.userList.add(user7);
-        this.userList.add(user8);
-        this.userList.add(user9);
-
-        return this.userList;
-    }
-    public User logIn(String email, String password){
+    public User logIn(String userEmail, String password){
         User user = null;
-        verificationOfUsers();
-        for (int i = 0; i < this.userList.size(); i++){
-            if (this.userList.get(i).getEmail().equalsIgnoreCase(email) && this.userList.get(i).getUserPassword().equalsIgnoreCase(password)){
-                user = this.userList.get(i);
-                return user;
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:src/database/database.db");
+            String sql1 = "SELECT * FROM userClient";
+            Statement statement = connection.createStatement();
+            ResultSet result1 = statement.executeQuery(sql1);
+            while(result1.next()){
+                int id = result1.getInt("id");
+                int points = result1.getInt("points");
+                int cups = result1.getInt("cups");
+                String email = result1.getString("email");
+                String userPassword = result1.getString("userPassword");
+                String nickName = result1.getString("nickName");
+                String gender = result1.getString("gender");
+                String name = result1.getString("name");
+                if ((email.equalsIgnoreCase(userEmail)) && (userPassword.equalsIgnoreCase(password))){
+                    user = new UserClient(id, points, cups, email, name, userPassword, nickName, gender);
+                    return user;
+                }
             }
+            String sql2 = "SELECT * FROM userAdmin";
+            ResultSet result2 = statement.executeQuery(sql2);
+            while(result2.next()){
+                int id = result2.getInt("id");
+                String email = result2.getString("email");
+                String userPassword = result2.getString("userPassword");
+                String nickName = result2.getString("nickName");
+                String gender = result2.getString("gender");
+                String name = result2.getString("name");
+                if ((email.equalsIgnoreCase(userEmail)) && (userPassword.equalsIgnoreCase(password))){
+                    user = new UserAdmin(id, email, name, userPassword, nickName, gender);
+                    return user;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return user;
     }
     public void singUp(String email, String name, String userPassword, String nickName, String gender){
-        verificationOfUsers();
         int size = this.userList.size()-1;
         this.userList.get(size);
         User user = new UserClient(size+1, 0, 0, email, name, userPassword, nickName, gender);
         userList.add(user);
         size = this.userList.size()-1;
-    }
-    public void verificationOfUsers(){
-        if (this.userList.size() == 0){
-            this.userList = instantiateUsers();
-        }
     }
 }
